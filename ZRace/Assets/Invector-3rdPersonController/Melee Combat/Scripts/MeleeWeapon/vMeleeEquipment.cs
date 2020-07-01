364ecd2dea1a5ad5b22723a71e9fd18fc1bd7ec5
mtarradas <mtarradas@gmail.com>
2020-07-01 12:31:01 +0200

    Invector Asset
    Add Invector

---
+++ ZRace/Assets/Invector-3rdPersonController/Melee Combat/Scripts/MeleeWeapon/vMeleeEquipment.cs
@@ -0,0 +1,44 @@
+﻿using Invector.vMelee;
+using UnityEngine;
+namespace Invector.vItemManager
+{
+    [vClassHeader("Melee Equipment", openClose = false, useHelpBox = true, helpBoxText = "Use this component if you also use the ItemManager in your Character")]
+    public class vMeleeEquipment : vEquipment
+    {
+        vMeleeWeapon _weapon;
+        bool withoutWeapon;
+
+        protected virtual vMeleeWeapon weapon
+        {
+            get
+            {
+                if (!_weapon && !withoutWeapon)
+                {
+                    _weapon = GetComponent<vMeleeWeapon>();
+                    if (!_weapon) withoutWeapon = true;
+                }
+
+                return _weapon;
+            }
+        }
+
+        public override void OnEquip(vItem item)
+        {
+            if (!weapon) return;
+            base.OnEquip(item);
+            var damage = item.GetItemAttribute(vItemAttributes.Damage);
+            var staminaCost = item.GetItemAttribute(vItemAttributes.StaminaCost);
+            var defenseRate = item.GetItemAttribute(vItemAttributes.DefenseRate);
+            var defenseRange = item.GetItemAttribute(vItemAttributes.DefenseRange);
+            if (damage != null) this.weapon.damage.damageValue = damage.value;
+            if (staminaCost != null) this.weapon.staminaCost = staminaCost.value;
+            if (defenseRate != null) this.weapon.defenseRate = defenseRate.value;
+            if (defenseRange != null) this.weapon.defenseRange = defenseRate.value;
+        }
+        public override void OnUnequip(vItem item)
+        {
+            if (!weapon) return;
+            base.OnUnequip(item);
+        }
+    }
+}
---
+++ ZRace/Assets/Invector-3rdPersonController/Shooter/Scripts/Weapon/vShooterEquipment.cs
@@ -0,0 +1,103 @@
+﻿using Invector.vShooter;
+using UnityEngine;
+
+namespace Invector.vItemManager
+{
+    [vClassHeader("Shooter Equipment", openClose = false, useHelpBox = true, helpBoxText = "Use this component if you also use the ItemManager in your Character")]
+    public class vShooterEquipment : vEquipment
+    {
+        vShooterWeapon _shooter;
+        vMelee.vMeleeWeapon _melee;
+        bool withoutShooterWeapon;
+        bool withoutMeleeWeapon;
+
+      
+        protected virtual vShooterWeapon shooterWeapon
+        {
+            get
+            {
+                if (!_shooter && !withoutShooterWeapon)
+                {
+                    _shooter = GetComponent<vShooterWeapon>();
+                    if (!_shooter) withoutShooterWeapon = true;
+                }
+
+                return _shooter;
+            }
+        }
+        protected virtual vMelee.vMeleeWeapon meleeWeapon
+        {
+            get
+            {
+                if (!_melee && !withoutMeleeWeapon)
+                {
+                    _melee = GetComponent<vMelee.vMeleeWeapon>();
+                    if (!_melee) withoutMeleeWeapon = true;
+                }
+
+                return _melee;
+            }
+        }
+        public override void OnEquip(vItem item)
+        {
+            if (!shooterWeapon) return;
+            base.OnEquip(item);
+            shooterWeapon.changeAmmoHandle = new vShooterWeapon.ChangeAmmoHandle(ChangeAmmo);
+            shooterWeapon.checkAmmoHandle = new vShooterWeapon.CheckAmmoHandle(CheckAmmo);
+            var damageAttribute = item.GetItemAttribute(shooterWeapon.isSecundaryWeapon ? vItemAttributes.SecundaryDamage : vItemAttributes.Damage);
+
+            if (damageAttribute != null)
+            {
+                shooterWeapon.maxDamage = damageAttribute.value;
+            }
+
+            if (shooterWeapon.secundaryWeapon)
+            {
+                var _equipments = shooterWeapon.secundaryWeapon.GetComponents<vEquipment>();
+                for (int i = 0; i < _equipments.Length; i++)
+                {
+                    if (_equipments[i] != null) _equipments[i].OnEquip(item);
+                }
+            }
+        }
+
+        public override void OnUnequip(vItem item)
+        {
+            if (!shooterWeapon) return;
+            base.OnUnequip(item);
+            if (!item) return;           
+            shooterWeapon.changeAmmoHandle = null;
+            shooterWeapon.checkAmmoHandle = null;
+
+            if (shooterWeapon.secundaryWeapon)
+            {
+                var _equipments = shooterWeapon.secundaryWeapon.GetComponents<vEquipment>();
+                for (int i = 0; i < _equipments.Length; i++)
+                {
+                    if (_equipments[i] != null) _equipments[i].OnUnequip(item);
+                }
+            }
+        }
+
+        protected virtual bool CheckAmmo(ref bool isValid, ref int totalAmmo)
+        {
+            if (!referenceItem) return false;
+            var damageAttribute = referenceItem.GetItemAttribute(shooterWeapon.isSecundaryWeapon ? vItemAttributes.SecundaryAmmoCount : vItemAttributes.AmmoCount);
+            isValid = damageAttribute != null && !damageAttribute.isBool;
+            if (isValid) totalAmmo = damageAttribute.value;
+            return isValid && damageAttribute.value > 0;
+        }
+
+        protected virtual void ChangeAmmo(int value)
+        {
+            if (!referenceItem) return;
+            var damageAttribute = referenceItem.GetItemAttribute(shooterWeapon.isSecundaryWeapon ? vItemAttributes.SecundaryAmmoCount : vItemAttributes.AmmoCount);
+
+            if (damageAttribute != null)
+            {
+                damageAttribute.value += value;
+            }
+        }
+
+    }
+}
